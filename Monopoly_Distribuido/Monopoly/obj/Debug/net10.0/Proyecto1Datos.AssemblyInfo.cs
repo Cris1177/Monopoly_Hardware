66e8f26438e55aefbf572efd58d92268505b160c
Voltor500 <sacjosue22@gmail.com>
2026-09-09 13:36:32 -0600

    provando la crecion de la nueva rama

--- Monopoly_Distribuido/Monopoly/obj/Debug/net10.0/Proyecto1Datos.AssemblyInfo.cs
+++ Monopoly_Distribuido/Monopoly/obj/Debug/net10.0/Proyecto1Datos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0863a98492a6c0b4e9f65296c42c8bc67c5eac3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0054c0f117c93f0f959bf653e9c6b0dcbc697e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
